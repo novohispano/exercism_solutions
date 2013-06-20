6ba1bacd68ac96b66192daf83b455a5109673654
novohispano@gmail.com <novohispano@gmail.com>
2013-06-19 18:11:22 -0600

    Anagram solution :)

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -2,47 +2,22 @@ class Anagram
   attr_reader :letters
 
   def initialize(word)
-    @letters = word_to_letters(word)
+    @letters = letters_from(word)
   end
 
   def match(words)
-    words_array = words_to_array(words)
-    anagram_finder(words_array)
+    words.select do |word|
+      words_match?(letters, letters_from(word))
+    end
   end
 
   private
 
-  def words_to_array(words)
-    words_array = []
-
-    words.each do |word|
-      words_array << word_to_letters(word)
-    end
-
-    words_array
+  def letters_from(word)
+    word.split("").sort
   end
 
-  def word_to_letters(word)
-    word.split("")
-  end
-
-  def array_to_word(array)
-    array.join
-  end
-
-  def array_matcher(array1, array2)
-    array1.sort == array2.sort
-  end
-
-  def anagram_finder(words_array)
-    match = []
-
-    words_array.each do |word|
-      if array_matcher(letters, word)
-        match << array_to_word(word)
-      end
-    end
-
-    match
+  def words_match?(word1, word2)
+    word1 == word2
   end
 end
