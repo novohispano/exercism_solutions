5b3563508bebd95b18bc8079ea0c488a2a8a82e5
novohispano <novohispano@gmail.com>
2013-11-14 15:08:21 -0700

    Create Fragment class on beer_song

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -35,7 +35,7 @@ class Verse
     if number == 0
       "Go to the store and buy some more, "
     else
-      "Take #{pluralizer} down and pass it around, "
+      "Take #{pronoun} down and pass it around, "
     end
   end
 
@@ -43,16 +43,16 @@ class Verse
     "#{remainder_bottles} of beer on the wall.\n"
   end
 
-  def pluralizer
-    number == 1 ? "it" : "one"
+  def pronoun
+    Fragments.new(number).pronoun
   end
 
   def remainder_bottles
-    Bottle.new(next_number).to_s
+    Fragments.new(next_number).bottles
   end
 
   def bottles
-    Bottle.new(number).to_s
+    Fragments.new(number).bottles
   end
 
   def next_number
@@ -60,21 +60,28 @@ class Verse
   end
 end
 
-class Bottle
+class Fragments
   attr_reader :number
 
   def initialize(number)
     @number = number
   end
 
-  def to_s
-    case number
-    when 0
-      "no more bottles"
-    when 1
-      "#{number} bottle"
-    else
-      "#{number} bottles"
-    end
+  def bottles
+    bottle_number + bottle_noun
+  end
+
+  def pronoun
+    number == 1 ? "it" : "one"
+  end
+
+  private
+
+  def bottle_noun
+    number == 1 ? "bottle" : "bottles"
+  end
+
+  def bottle_number
+    number == 0 ? "no more " : "#{number} "
   end
 end
